968f3c9bcfcc05226c1522c11447051eb8e22841
zackkkkkkkkk <zack.natakit@gmail.com>
2023-01-20 11:40:35 +0700

    commit all

---
+++ public/index.js
@@ -0,0 +1,7 @@
+function to_Github() {
+  window.location.href = "https://github.com/zacntk";
+}
+
+function to_GGDrive() {
+    window.location.href = "https://drive.google.com/drive/folders/1xlHU6w604IwEcSCp1oglMBUJcoXwq63a?usp=share_link";
+  }
